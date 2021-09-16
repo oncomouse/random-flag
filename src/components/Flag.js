666dff42c6194d95dcd2602c5827cba0be9527eb
oncomouse <oncomouse@gmail.com>
2021-09-16 14:20:50 -0500

    Randomly choose either black or white; better abstraction

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -9,51 +9,58 @@ const FLAG_HEIGHT = 400;
 
 const randomFromArray = arr => arr[Math.floor(Math.random() * arr.length)];
 
-const generateColors = [
-	(len) => {
-		const bucket = palettes[Math.floor(Math.random() * palettes.length)].slice();
-		if (len === 5) {
-			return bucket;
-		}
-		if (len < 5) {
-			return Array(len).fill(null).map(() => {
-				console.log(bucket)
-				return bucket.splice(Math.floor(Math.random() * bucket.length), 1)[0];
-			})
-		}
-		// What to do for bigger than 5?
-	},
-	(len) => Array(len).fill(null).map(() => randomColor(Math.random(), Math.random()).hexString())
-]
+const generateColors = len => {
+	return randomFromArray([
+		(len) => {
+			const bucket = randomFromArray(palettes).slice();
+			if (len === 5) {
+				return bucket;
+			}
+			if (len < 5) {
+				return Array(len).fill(null).map(() => {
+					return bucket.splice(Math.floor(Math.random() * bucket.length), 1)[0];
+				})
+			}
+			// What to do for bigger than 5?
+		},
+		(len) => Array(len).fill(null).map(() => randomColor(Math.random(), Math.random()).hexString())
+	])(len);
+}
 
 const getColor = (colors, color_index) => {
 	if (color_index === -1) {
-		return "#000000";
-	} else if (color_index === 0) {
-		return "#FFFFFF";
+		return '#000000';
+	}
+	if (color_index === 0) {
+		return '#FFFFFF';
+	}
+	if (color_index === -2) {
+		return randomFromArray(['#000000', '#FFFFFF']);
 	}
 	return colors[color_index - 1];
 }
 
-const flagToShapes = (flag, colors) => flag.shapes.map((s, i) => (
-	<Line
-		key={i}
-		points={s.points.map(([x,y]) => [(x/100) * FLAG_WIDTH, (y/100) * FLAG_HEIGHT]).flat()}
-		closed
-		fill={getColor(colors, s.color)}
-		strokeWidth={2}
-		stroke={getColor(colors, s.color)}>
-	</Line>
-));
+const flagToShapes = (flag, colors) => flag.shapes.map((s, i) => {
+	const color = getColor(colors, s.color);
+	return (
+		<Line
+			key={i}
+			points={s.points.map(([x,y]) => [x * FLAG_WIDTH, y * FLAG_HEIGHT]).flat()}
+			closed
+			fill={color}
+			strokeWidth={2}
+			stroke={color}>
+		</Line>
+	)
+});
 
 const Flag = ({ reRender }) => {
 	const flag = useRef(randomFlag());
-	const colors = useRef(randomFromArray(generateColors)(flag.current.colors));
+	const colors = useRef(generateColors(flag.current.colors));
 	useEffect(() => {
 		flag.current = randomFlag();
-		colors.current = randomFromArray(generateColors)(flag.current.colors);
+		colors.current = generateColors(flag.current.colors);
 	}, [reRender])
-	console.log(colors);
 	return (
 		<Stage width={FLAG_WIDTH} height={FLAG_HEIGHT}>
 			<Layer>
