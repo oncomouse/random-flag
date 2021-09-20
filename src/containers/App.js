5e8dd043450fba2b1f409a5e94996e93255befca
oncomouse <oncomouse@gmail.com>
2021-09-20 11:46:50 -0500

    Added cutestrap and started styling for it.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Flag from '../components/Flag';
 import Debug from '../components/Debug';
+import 'cutestrap/dist/css/cutestrap.css';
 import './App.css';
 
 const DEBUG = true;
@@ -9,11 +10,9 @@ const App = () => {
 	const [count, updateCount] = useState(0);
 	const [debug, updateDebug] = useState({});
 	return (
-		<div className="App">
-			<Flag reRender={count} debug={debug}/>
-			<div className="controls">
-				<button onClick={updateCount}>New Flag</button>
-			</div>
+		<div className="wrapper -wide ta-center">
+				<Flag reRender={count} debug={debug}/>
+				<button onClick={updateCount} class="centered-button new-flag-button">New Flag</button>
 			{ DEBUG ? <Debug update={updateDebug} /> : null }
 		</div>
 	);
