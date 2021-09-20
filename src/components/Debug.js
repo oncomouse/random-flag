f90b740b9519eba7ec30d7f72eab23deab3f3285
oncomouse <oncomouse@gmail.com>
2021-09-20 14:57:52 -0500

    More work on the debugger

--- src/components/Debug.js
+++ src/components/Debug.js
@@ -24,8 +24,8 @@ const Debug = (props) => {
 	} = props;
 	const { register, watch, handleSubmit } = useForm();
 	const onSubmit = data => console.log(data);
-	const [custom, updateCustom] = useState(false);
-	const [type, updateType] = useState('line');
+	const templateName = watch('name', 'random');
+	const customType = watch('custom.type', 'line');
 	useEffect(() => {
 		const subscription = watch((value, { name, type }) => {
 			if (name === 'name' &&  type === 'change') {
@@ -33,24 +33,16 @@ const Debug = (props) => {
 					...debug,
 					name: value.name,
 				})
-				if (value.name !== 'custom') {
-					if (custom) {
-						updateCustom(false);
-					}
-				} else {
-					updateCustom(true);
-				}
 			}
-			console.log(value, name, type);
 		});
 		return () => subscription.unsubscribe();
-	}, [watch, update, custom, updateCustom]);
-	return (<div className="debug-controls">
+	}, [watch, update]);
+	return <div className="debug-controls">
 		<form onSubmit={handleSubmit(onSubmit)}>
 			<RegisterContext.Provider value={register}>
 				<h1>Debug Controls</h1>
 				<label className="field">
-					<select id="name" {...register('name', { value: 'random', })}>
+					<select id="name" {...register('name', { defaultValue: 'random' })}>
 						<option value="random">Random (default behavior)</option>
 						{flags.map((flag, i) => (
 							<option key={i} value={flag.name}>{flag.name}</option>
@@ -59,9 +51,54 @@ const Debug = (props) => {
 					</select>
 					<span className="label">Which Flag Template to Render:</span>
 				</label>
+				{ templateName === 'custom' ? <CustomShapeForm type={customType} /> : null }
 			</RegisterContext.Provider>
 		</form>
-	</div>);
+	</div>;
+}
+
+const CustomShapeForm = (props) => {
+	const {
+		types,
+	} = props;
+
+	const register = useContext(RegisterContext);
+
+	return (
+		<div>
+			<label className="field">
+				<input {...register('custom.name')} />
+				<span className="label">Template Name:</span>
+			</label>
+			<label className="field">
+				<input type="number" min="0" {...register('custom.colors', { min: 0 })}/>
+				<span className="label">Number of Colors:</span>
+			</label>
+			<section className="grid">
+				<div>
+					<label className="field">
+						<select {...register('custom.type')}>
+							<option value="line">Line</option>
+							<option value="star">Star</option>
+							<option value="stripes">Stripes</option>
+							<option value="circle">Circle</option>
+						</select>
+						<span className="label">Type of Shape</span>
+					</label>
+
+					<button className="button -center">Add Another Shape</button>
+				</div>
+				<div>
+					<select multiple style={{ width: '100%' }}>
+					</select>
+					<section className="grid">
+						<div><button className="button">Move Up</button></div>
+						<div><button className="button">Move Down</button></div>
+					</section>
+					</div>
+			</section>
+		</div>
+	)
 }
 
 export default Debug;
